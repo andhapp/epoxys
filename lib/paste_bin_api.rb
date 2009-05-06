70360efaba93dc5565a076e97b82456eefd76d2c
Anuj Dutta <dutta.anuj@gmail.com>
2009-05-06 20:40:26 +0100

    Added comments to support the code

--- lib/paste_bin_api.rb
+++ lib/paste_bin_api.rb
@@ -7,7 +7,7 @@ class PasteBinApi
 
 		attr_reader :post_detail
 
-		# To define methods like paste_create, paste_id, paste_url 
+		# To define methods like paste_create_new, paste_id, paste_url 
 		# The first conditional deals with paste_create and the second one with paste_id, paste_url 
 		def method_missing(name, *args, &block)
 			if name.to_s =~ /^(paste_)(.*)$/ and !respond_to?(name)
@@ -16,7 +16,7 @@ class PasteBinApi
 					server = XMLRPC::Client.new2(paste_bin.url)
 					@post_detail = server.call2(paste_bin.post_method, paste_bin.code, paste_bin.syntax, paste_bin.author, paste_bin.description, paste_bin.is_private, paste_bin.password, paste_bin.secret_key, paste_bin.api_key)
 				else
-					@post_detail[1]["#{$2.upcase}"]
+					@post_detail[1]["#{$2.upcase}"] # the postdetails object is supplied back like this [true, ["UEL" => "", "ID" => ""]]
 				end
 			end
 			
