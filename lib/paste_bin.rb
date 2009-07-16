735ad0866414bc03abb264c6e0e87bf5de28377d
anuj  dutta <andhapp@andHapps-MacBook.local>
2009-07-17 02:21:39 +0530

    Added or conditional to deal with nil values

--- lib/paste_bin.rb
+++ lib/paste_bin.rb
@@ -5,13 +5,13 @@ class PasteBin
 	def initialize(url='', post_method='', code='', syntax='ruby', author='', description='', is_private=false, password='', secret_key='', api_key=3)
 		@url = url
 		@post_method = post_method
-		@code = code
+		@code = code || '' #(OR - if the first condition is nil (i.e. false in ruby) then it would take the second value otherwise it would take the first value)
 		@syntax = syntax
-		@author = author
-		@description = description
+		@author = author || ''
+		@description = description  || ''
 		@is_private = is_private
-		@password = password
-		@secret_key = secret_key
+		@password = password  || ''
+		@secret_key = secret_key  || ''
 		@api_key = api_key
 	end
 	
