be43053e4c59cee99b78f16119eb263e2d469b1b
anuj  dutta <andhapp@andHapps-MacBook.local>
2009-07-17 02:21:18 +0530

    Made the is_private as default false bcoz the user cannot specify the password and paste cannot really be private without a password

--- epoxys.rb
+++ epoxys.rb
@@ -10,7 +10,7 @@ end
 post '/paste/create' do
 	request_url = 'http://openpaste.org/api/xml-rpc/'
 	post_method = 'Openpaste.addPost'
-	is_private = params[:is_private].eql?("false") ? false : true # the parameters are converted to strings
+	is_private = false #leave it false for default as the user is not allowed to enter the password and one cannot have a private paste without a password ;)
 	PasteBinApi.paste_create_new(request_url, post_method, params[:code], params[:syntax], params[:author], params[:description], is_private, params[:password], params[:secret_key])
 	PasteBinApi.paste_url
 end
